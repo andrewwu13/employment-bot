d5e67c70d7114f71f36a5ac74a05919e1104ce23
Ian Yeh <ianyeh7@gmail.com>
2025-11-29 01:11:38 -0500

    init db and some scraping

--- src/services/scrapeService.js
+++ src/services/scrapeService.js
@@ -1,7 +1,14 @@
 // chromium - headless browser
 import { chromium } from 'playwright';
+import { mockPostings } from './mockData.js';
+import { db } from '../config/firebaseConfig.js';
+import { collection, addDoc } from 'firebase/firestore';
 
-export async function scrape() {
+export async function scrape(jobPostings) {
+  /**
+   * Scrapes a website and returns the page title and text of a specific element.
+   * @param {object} jobPostings - An object containting the company title, and the job posting link. 
+   */
   const browser = await chromium.launch();
 
   // setting up a user agent to avoid detection 
@@ -9,17 +16,47 @@ export async function scrape() {
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
   });
 
-  const page = await context.newPage();
-  await page.goto('https://www.scrapethissite.com');
+  for (let posting of jobPostings) {
+    try {
+      console.log(`Scraping job posting for ${posting.company} at ${posting.postingLink}`);
+      const page = await context.newPage();
+      await page.goto(posting.postingLink, { waitUntil: 'domcontentloaded' });
 
-  const pageTitle = await page.title()
-  console.log(pageTitle)
+      const pageTitle = await page.title()
+      console.log(pageTitle)
 
-  const elementText = await page.$eval('h1', el => el.textContent); 
-  console.log(elementText);
+      const elementText = await page.$eval('h1', el => el.textContent); 
+      console.log(elementText);
+
+      console.log(pageTitle, elementText);
+
+      addDbEntry({ company: posting.company, postingLink: posting.postingLink, title: pageTitle, content: elementText });
+
+    } catch (error) {
+      console.error(`Error scraping ${posting.company}:`, error.message);
+      // Continue to next posting even if this one fails
+    }
+  }
 
   await browser.close();
-
-  return { pageTitle, elementText }
 }
 
+async function addDbEntry(data) {
+  /**
+   * Adds an entry to the Firestore database.
+   * @param {object} data - The data to be added to the database.
+   */
+
+  try {
+    const docRef = await addDoc(collection(db, "job_postings"), data);
+    console.log("Document written with ID: ", docRef.id);
+  } catch (error) {
+    console.error("Error adding document: ", error); 
+  }
+
+} 
+
+// run for node testing in isolation
+if (import.meta.url === `file://${process.argv[1]}`) {
+  scrape(mockPostings).catch(console.error);
+}
