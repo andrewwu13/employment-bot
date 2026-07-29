da44536da9776ffa8931920d342fd9cb057cf758
Ian Yeh <ianyeh7@gmail.com>
2026-07-29 09:54:13 -0400

    chore: add function stub to ai package

--- packages/shared/src/index.js
+++ packages/shared/src/index.js
@@ -1,6 +1,3 @@
-export * from './constants.js';
 export { Job } from './Job.js';
 export { Logger } from './logger.js';
-
-// exporting constants
 export { SITE_HANDLERS, allSkills, COOKIE_DISMISS_SELECTORS } from './constants.js';
