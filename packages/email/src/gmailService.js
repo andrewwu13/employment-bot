231e1460a57e7270f44cbd8cd0dee617893e01ec
Ian Yeh <ianyeh7@gmail.com>
2026-09-08 11:51:32 -0400

    rename gmail service files

--- packages/email/src/gmailService.js
+++ packages/email/src/gmailService.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import * as cheerio from 'cheerio';
 
 // gmail object created from OAuth process; can work with it now to read/send gmails
-import { gmail } from './gmail-config.js';
+import { gmail } from './gmail.config.js';
 import { Logger } from '@repo/shared';
 
 dotenv.config();
